fb5b097cb9547762e68739e08366608c838db534
Prescott Murphy <prurph@gmail.com>
2014-04-12 11:23:41 -0400

    DOM templates with restrict, inline and external templates, isolate scope

--- js/ch3/ch3DomManipulation.js
+++ js/ch3/ch3DomManipulation.js
@@ -12,6 +12,41 @@ app.directive("myWidget", function() {
 
   return {
     restrict: "E",
-    link: linkFunction
+    link: linkFunction,
+  };
+});
+
+app.directive("myTemplateWidget", function() {
+  return {
+    restrict: "E",
+    templateUrl: "my_template_widget.html",
+    replace: true
+  };
+});
+
+// transclude: true with ng-transclude means insert existing nodes as children
+// of the ng-transclude element
+app.directive("myTranscludeWidget", function() {
+  return {
+    restrict: "E",
+    transclude: true,
+    template: "<div ng-transclude><h3>Heading</h3></div>"
+  };
+});
+
+app.directive("myConfigWidget", function() {
+  var linkFunction = function(scope, element, attributes) {
+    // either dot notation or bracket notation works
+    scope.text  = attributes["myConfigWidget"];
+    scope.text2 = attributes.anotherAttribute;
+  };
+
+  return {
+    restrict: "A",
+    template: "<p>{{text}} {{text2}}</p>",
+    link: linkFunction,
+    // this is called "isolate scope": makes sure that variables in this scope
+    // don't overwrite identically named variables in parent scope
+    scope: {}
   };
 });
